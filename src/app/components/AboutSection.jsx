0a7818b51803d24d2166775528175860d094c537
saivinaybolloju <saivinay.bolloju@gmail.com>
2025-10-06 01:22:32 +0530

    About Section and Global CSS Page

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,44 @@
+"use client"
+import React from 'react'
+import Image from "next/image";
+
+export default function AboutSection() {
+  return (
+    <>
+        <section id='about' className='py-20 px-6 md:px-20 bg-white text-center'>
+            <p className='text-gray-500 font-medium text-lg'>Get to Know More</p>
+            <h1 className='text-4xl font-bold mb-10'>About me</h1>
+
+            <div className='flex flex-col md:flex-row items-center justify-center gap-12'>
+                <div className='flex flex-row sm:flex-row gap-6 justify-center'>
+                    <div className='flex flex-row items-center border border-gray-300 rounded-2xl p-6 w-full sm:w-1/2 shadow-sm'>
+                        <Image src="/assests/profilepic.png" alt="Experience icon" width={40} height={40} className="mb-3"></Image>
+                        <h3 className='text-xl font-semibold'>Experience</h3>
+                        <p className='text-gray-600'>1 year Frontend Development</p>
+                    </div>
+                    <div className='flex flex-col items-center border border-gray-300 rounded-2xl p-6 w-full sm:w-1/2 shadow-sm'>
+                        <Image src="/assests/profilepic.png" alt="Experience icon" width={40} height={40} className="mb-3"></Image>
+                        <h3 className='text-xl font-semibold'>Education</h3>
+                        <p className='text-gray-600'>Computer Science Engineering</p>
+                    </div>
+                    <br />
+                    <br />
+                    <p className='text-gray-700 leading-relaxed'>
+                        I'm a passionate developer with a strong focus on creating user-friendly
+            and efficient applications. My journey in tech revolves around learning,
+            experimenting, and constantly improving my skills. I love exploring
+            frontend technologies and turning creative ideas into working products.
+                    </p>
+
+                </div>
+
+            </div>
+
+        </section>
+        <h3><center>Stay Tuned Coming in...</center></h3>
+      <br /><br /><br />
+
+
+    </>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import HeroSection from './components/HeroSection';
+import AboutSection from './components/AboutSection';
 export default function Home() {
   return (
     <div>
       <Navbar/>
       <HeroSection/>
-      <h1>Hello World</h1>
+      <AboutSection/>
+      
     </div>
   );
 }
